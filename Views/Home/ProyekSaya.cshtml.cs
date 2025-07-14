4c83c07e95433652ee29b1916c2ed9b931e8acfe
Jali Suhaman <86903939+Jalil98@users.noreply.github.com>
2025-07-15 01:19:05 +0700

    Add files via upload

---
+++ Views/Home/ProyekSaya.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portofolio.Views.Home
+{
+    public class ProyekSayaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
